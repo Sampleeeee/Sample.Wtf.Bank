bf5e11740ed5af8b0abcdea486899d88d6713858
sampleeeee <behnersam@gmail.com>
2024-11-03 01:43:42 -0400

    Account Details Page

--- Sample.Wtf.Bank.App/Pages/Accounts.razor.cs
+++ Sample.Wtf.Bank.App/Pages/Accounts.razor.cs
@@ -2,7 +2,7 @@ using Microsoft.AspNetCore.Components;
 
 namespace Sample.Wtf.Bank.App.Pages;
 
-public partial class Account
+public partial class Accounts
 {
     [Parameter] public string UserId { get; set; }
 }
